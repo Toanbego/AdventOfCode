2df9c83ff1c02cf3c210d439d3b6fbc20f794e1d
SYSCOMWORLD\torstein.gombos <tg@syscomworld.com>
2020-12-03 15:21:02 +0100

    Cleaned up code in Day 3

--- Python/Day3/toboggan_trajectory.py
+++ Python/Day3/toboggan_trajectory.py
@@ -18,30 +18,6 @@ def format_data(data):
     """
     return np.array([np.array(list(line)) == '#' for line in data])
 
-def print_position(map_snapshot, next_map_snapshot, position, counter):
-    """
-
-    :param map_snapshot:
-    :param position:
-    :return:
-    """
-    map_snapshot = map_snapshot*1
-    next_map_snapshot = next_map_snapshot*1
-    next_map_snapshot = next_map_snapshot.astype(str)
-    # map_snapshot = list(map_snapshot)
-    if map_snapshot[position[1]] == 1:
-        map_snapshot = map_snapshot.astype(str)
-        map_snapshot[position[1]] = 'T'
-    else:
-        map_snapshot = map_snapshot.astype(str)
-        map_snapshot[position[1]] = 'X'
-    next_map_snapshot = next_map_snapshot.astype(str)
-
-
-    print(*map_snapshot , sep=", ")
-    print(*next_map_snapshot, sep=", ")
-    print("-----------------------------------------------")
-
 
 def count_trees_in_path(toboggan_map, path=(3, 1)):
     """
@@ -50,35 +26,25 @@ def count_trees_in_path(toboggan_map, path=(3, 1)):
     :param path: path to take trough the map
     :return:
     """
-
     tree_count = 0
     position = [0, 0]
-
-    i = 0
     map_length_east = len(toboggan_map[0])
     map_length_south = len(toboggan_map)
 
-    # Traverse downhill
     while position[0] < map_length_south:
-        # print_position(toboggan_map[position[0]], toboggan_map[position[0]+1], position, counter=tree_count)
 
-        # Check for trees
         if toboggan_map[position[0]][position[1]]:
             tree_count += 1
-        next_position = [position[0] + path[1], position[1] + path[0]]
-        if next_position[1] > map_length_east-1:
-            next_position[1] = next_position[1]-map_length_east
 
-        position = next_position
+        position = [position[0] + path[1], (position[1] + path[0]) % map_length_east]
 
     return tree_count
-
-
 
 if __name__ == '__main__':
     puzzle_input = read_data('puzzle_input.txt')
     puzzle_input = format_data(puzzle_input)
 
+    # Check all paths for trees.
     paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     trees_per_path_product = 1
     for path in paths:
