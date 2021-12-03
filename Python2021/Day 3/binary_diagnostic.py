972c279a492c681c8a1212c851e7eac42231be06
Gombos <torstein.gombos@dxc.com>
2021-12-03 15:33:14 +0100

    Added solution for day 3

--- Python2021/Day 3/binary_diagnostic.py
+++ Python2021/Day 3/binary_diagnostic.py
@@ -13,6 +13,35 @@ def binatodeci(binary):
     return sum(val*(2**idx) for idx, val in enumerate(reversed(binary)))
 
 
+def calculate_lifesupport(binary, life_support_type):
+    if life_support_type == "O2":
+        bit1, bit2 = 1, 0
+    elif life_support_type == "CO2":
+        bit1, bit2 = 0, 1
+    sequence_length = len(binary[0])
+    binary_length = len(binary.transpose()[0])
+    i = 0
+    while True:
+        if binary_length == 1:
+            break
+
+        bit_position = binary.transpose()[i]
+        if len(bit_position[bit_position == 1]) >= len(bit_position[bit_position == 0]):
+            binary_length = len(np.argwhere(bit_position == bit1))
+            binary = binary[np.argwhere(bit_position == bit1)].reshape(binary_length,
+                                                                       sequence_length)
+        else:
+            binary_length = len(np.argwhere(bit_position == bit2))
+            binary = binary[np.argwhere(bit_position == bit2)].reshape(binary_length,
+                                                                       sequence_length)
+
+        if i > sequence_length:
+            i = 0
+        else:
+            i += 1
+    return binary[0]
+
+
 def task1(data):
     """Write the code for task 1 here"""
     binary_array = np.stack([[int(binary) for binary in sequence] for sequence in data])
@@ -32,53 +61,8 @@ def task1(data):
 def task2(data):
     """Write the code for task 2 here"""
     binary_array = np.stack([[int(binary) for binary in sequence] for sequence in data])
-    oxygen_generator_rating, CO2_scrubber_rating = deepcopy(binary_array), deepcopy(binary_array)
-    sequence_length = len(binary_array[0])
-    binary_length = len(binary_array.transpose()[0])
+    return binatodeci(calculate_lifesupport(binary_array, "O2")) * binatodeci(calculate_lifesupport(binary_array, "CO2"))
 
-    i = 0
-    while(True):
-        if binary_length == 1:
-            i = 0
-            binary_length = len(binary_array.transpose()[0])
-            break
-
-        bit_position = oxygen_generator_rating.transpose()[i]
-        if len(bit_position[bit_position == 1]) >= len(bit_position[bit_position == 0]):
-            binary_length = len(np.argwhere(bit_position == 1))
-            oxygen_generator_rating = oxygen_generator_rating[np.argwhere(bit_position == 1)].reshape(binary_length,
-                                                                                                      sequence_length)
-        else:
-            binary_length = len(np.argwhere(bit_position == 0))
-            oxygen_generator_rating = oxygen_generator_rating[np.argwhere(bit_position == 0)].reshape(binary_length,
-                                                                                                      sequence_length)
-
-        if i > sequence_length:
-            i = 0
-        else:
-            i += 1
-
-
-    while(True):
-        if binary_length == 1:
-            break
-
-        bit_position = CO2_scrubber_rating.transpose()[i]
-        if len(bit_position[bit_position == 1]) >= len(bit_position[bit_position == 0]):
-            binary_length = len(np.argwhere(bit_position == 0))
-            CO2_scrubber_rating = CO2_scrubber_rating[np.argwhere(bit_position == 0)].reshape(binary_length,
-                                                                                              sequence_length)
-        else:
-            binary_length = len(np.argwhere(bit_position == 1))
-            CO2_scrubber_rating = CO2_scrubber_rating[np.argwhere(bit_position == 1)].reshape(binary_length,
-                                                                                              sequence_length)
-
-        if i > sequence_length:
-            i = 0
-        else:
-            i += 1
-
-    return binatodeci(oxygen_generator_rating[0]) * binatodeci(CO2_scrubber_rating[0])
 
 def main():
     puzzle_input = read_data()
